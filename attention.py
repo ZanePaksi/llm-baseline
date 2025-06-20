f56bcd86821a3c0970cb20404e82a7b386d93ee6
ZanePaksi <zane.paksi@outlook.com>
2025-06-20 17:08:35 -0400

    moved through casual attention

--- attention.py
+++ attention.py
@@ -1,5 +1,5 @@
 import torch
-
+from numpy.ma.core import masked
 
 inputs = torch.tensor(
         [[0.43, 0.15, 0.89],  # Your     (x^1)
@@ -114,6 +114,141 @@ def calc_weighted_attn_2():
     attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5, dim=-1)
     print(f"{attn_weights_2=}")
 
-    # Ending just before 3.4.2
 
-calc_weighted_attn_2()
+class SelfAttentionV1(torch.nn.Module):
+    """
+        Section 3.4.2
+        d_in and d_out represent the input embedding size
+        I.E The dimensionality of the vectors
+    """
+
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.w_query = torch.nn.Parameter(torch.rand(d_in, d_out))
+        self.w_key = torch.nn.Parameter(torch.rand(d_in, d_out))
+        self.w_value = torch.nn.Parameter(torch.rand(d_in, d_out))
+
+    def forward(self, embedded_tokens):
+        keys = embedded_tokens @ self.w_key
+        queries = embedded_tokens @ self.w_query
+        values = embedded_tokens @ self.w_value
+        # The book comments attn_scores with 'omega'
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+def try_SelfAttentionV1():
+    d_in = inputs.shape[1]
+    d_out = 2
+    torch.manual_seed(123)
+    sa_v1 = SelfAttentionV1(d_in, d_out)
+    print(sa_v1(inputs))
+
+
+class SelfAttentionV2(torch.nn.Module):
+
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.w_query = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.w_key = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.w_value = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, embedded_tokens):
+        keys = self.w_key(embedded_tokens)
+        queries = self.w_query(embedded_tokens)
+        values = self.w_value(embedded_tokens)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+def try_SelfAttentionV2():
+    d_in = inputs.shape[1]
+    d_out = 2
+    torch.manual_seed(789)
+    sa_v2 = SelfAttentionV2(d_in, d_out)
+    return sa_v2
+
+
+def casual_attention_start():
+    sa_v2 = try_SelfAttentionV2()
+    queries = sa_v2.w_query(inputs)
+    keys = sa_v2.w_key(inputs)
+    attn_scores = queries @ keys.T
+    attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+    print(attn_weights)
+
+    context_length = attn_scores.shape[0]
+    mask_simple = torch.tril(torch.ones(context_length, context_length))
+    print(mask_simple)
+
+    mask_simple = attn_weights * mask_simple
+    print(mask_simple)
+
+    row_sums = mask_simple.sum(dim=-1, keepdim=True)
+    masked_simple_norm = mask_simple / row_sums
+    print(masked_simple_norm)
+
+    print(f"\n masking to negative infinity")
+    mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+    masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+    print(masked)
+
+    attn_weights = torch.softmax(masked / keys.shape[-1]**0.5, dim=-1)
+    print(attn_weights)
+
+    # applying dropout to attn_weights
+    torch.manual_seed(123)
+    dropout = torch.nn.Dropout(0.5)
+    print(dropout(attn_weights))
+
+
+def casual_attention_dropout_example():
+    torch.manual_seed(123)
+    dropout = torch.nn.Dropout(0.5)
+    example = torch.ones(6, 6)
+    print(dropout(example))
+
+
+class CasualAttention(torch.nn.Module):
+
+    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.w_query = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.w_key = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.w_value = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1))
+
+    def forward(self, embedded_tokens):
+        b, num_tokens, d_in = embedded_tokens.shape
+        keys = self.w_key(embedded_tokens)
+        queries = self.w_query(embedded_tokens)
+        values = self.w_value(embedded_tokens)
+
+        attn_scores = queries @ keys.transpose(1, 2)
+        attn_scores.masked_fill_(self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+def casual_attention_implementation():
+    d_in = inputs.shape[1]
+    d_out = 2
+    batch = torch.stack((inputs, inputs), dim=0)
+    torch.manual_seed(123)
+    context_length = batch.shape[1]
+    ca = CasualAttention(d_in, d_out, context_length, 0.0)
+    context_vecs = ca(batch)
+    print("context_vecs.shape:", context_vecs.shape)
+
+casual_attention_implementation()
+
+# wrapping up at 3.6
