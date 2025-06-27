3aead46eb6fdea8ba0685304e71547cee5e50fec
Zane Paksi <zane.paksi@outlook.com>
2025-06-27 17:01:01 -0400

    adding start to text generation loss calculations

--- text_generation.py
+++ text_generation.py
@@ -34,4 +34,33 @@ def test_text():
 
     print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
 
-test_text()
+
+def text_gen_loss():
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    inputs = torch.tensor([[16833, 3626, 6100],  # ["every effort moves",
+                           [40, 1107, 588]])  # "I really like"]
+
+    targets = torch.tensor([[3626, 6100, 345],  # [" effort moves you",
+                            [1107, 588, 11311]])  # " really like chocolate"]
+
+    torch.manual_seed(123)
+    model = GPTModel(GPT_CONFIG_124M)
+    model.eval()
+
+    with torch.no_grad():
+        logits = model(inputs)
+
+    probas = torch.softmax(logits, dim=-1)
+    print(probas.shape)
+
+    token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+    print(f"{token_ids=}")
+
+    print(f"Targets batch 1: {token_ids_to_text(targets[0], tokenizer)}")
+    print(f"Outputs batch 1:"
+          f" {token_ids_to_text(token_ids[0].flatten(), tokenizer)}")
+
+
+text_gen_loss()
+
